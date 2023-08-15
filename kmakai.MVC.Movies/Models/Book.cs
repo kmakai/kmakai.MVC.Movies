09514d7d787bf0ffaa5e636c39f0166c0e201198
kmakai <makaikwag@gmail.com>
2023-08-15 15:27:41 -0400

    Add project files.

---
+++ kmakai.MVC.Movies/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace kmakai.MVC.Movies.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public DateTime Published { get; set; }
+    public string Genre { get; set; }
+}
