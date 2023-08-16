30388688406f87f5efe32fed3596565758c9bcea
kmakai <makaikwag@gmail.com>
2023-08-15 20:46:12 -0400

    Ready for review submission

--- kmakai.MVC.Movies/Controllers/HomeController.cs
+++ kmakai.MVC.Movies/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using kmakai.MVC.Movies.Models;
+﻿using kmakai.MVC.Movies.DataAccess;
+using kmakai.MVC.Movies.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +8,21 @@ namespace kmakai.MVC.Movies.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var movies = _context.Movies.ToList();
+            var books = _context.Books.ToList();
+            ViewData["Movies"] = movies;
+            ViewData["Books"] = books;
+
             return View();
         }
 
